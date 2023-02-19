ed21aed3d11896faf20e12726e5a7aa87b7ea2d1
paulodetarso.teles@hotmail.com <potassio@hotmail.com>
2023-02-19 19:36:18 -0300

    Finalizada a parte de inserir Facção, GetById(Detalhes) e listagem.

--- RemessaFaccao/RemessaFaccao.DAL/Repositories/FaccaoRepository.cs
+++ RemessaFaccao/RemessaFaccao.DAL/Repositories/FaccaoRepository.cs
@@ -17,27 +17,27 @@ namespace RemessaFaccao.DAL.Repositories.Interfaces
 
         public List<Faccao> GetAll()
         {
-            List<Faccao> result= new List<Faccao>();
-            SqlConnection connection = new SqlConnection(_connection.SQLString); 
+            List<Faccao> result = new List<Faccao>();
+            SqlConnection connection = new SqlConnection(_connection.SQLString);
 
-            using(connection)
+            using (connection)
             {
                 try
                 {
-                    connection.Open(); 
-                    SqlCommand command = new SqlCommand("SELECT FaccaoId, Nome, Ativo FROM dbo.Faccao; "); 
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT FaccaoId, Nome, Ativo FROM dbo.Faccao; ");
                     command.Connection = connection;
-                    command.CommandType = CommandType.Text; 
-                    SqlDataReader reader = command.ExecuteReader(); 
+                    command.CommandType = CommandType.Text;
+                    SqlDataReader reader = command.ExecuteReader();
 
                     while (reader.Read())
                     {
-                        result.Add( new Faccao
+                        result.Add(new Faccao
                         {
-                            FaccaoId = Convert.ToInt32(reader["FaccaoId"]), 
-                            Nome = reader["Nome"].ToString(), 
+                            FaccaoId = Convert.ToInt32(reader["FaccaoId"]),
+                            Nome = reader["Nome"].ToString(),
                             Ativo = Convert.ToBoolean(reader["Ativo"])
-                        }); 
+                        });
                     }
                 }
                 catch (Exception e)
@@ -55,19 +55,19 @@ namespace RemessaFaccao.DAL.Repositories.Interfaces
 
         public Faccao GetById(int id)
         {
-            Faccao result = new Faccao(); 
-            SqlConnection connection = new SqlConnection(_connection.SQLString); 
+            Faccao result = new Faccao();
+            SqlConnection connection = new SqlConnection(_connection.SQLString);
 
-            using(connection)
+            using (connection)
             {
                 try
                 {
                     connection.Open();
-                    SqlCommand command = new SqlCommand("dbo.FaccaoGetById"); 
+                    SqlCommand command = new SqlCommand("dbo.FaccaoGetById");
                     command.Connection = connection;
-                    command.CommandType = CommandType.StoredProcedure; 
+                    command.CommandType = CommandType.StoredProcedure;
                     command.Parameters.Add("@FaccaoId", SqlDbType.Int).Value = id;
-                    SqlDataReader reader = command.ExecuteReader(); 
+                    SqlDataReader reader = command.ExecuteReader();
 
                     if (reader.Read())
                     {
@@ -76,11 +76,11 @@ namespace RemessaFaccao.DAL.Repositories.Interfaces
                         result.Endereco = reader["Endereco"].ToString();
                         result.Email = reader["Email"].ToString();
                         result.Telefone1 = reader["Telefone1"].ToString();
-                        result.Telefone1 = reader["Telefone2"].ToString(); 
+                        result.Telefone1 = reader["Telefone2"].ToString();
                         result.FormaDePagamento = reader["FormaDePagamento"].ToString();
-                        result.Qualificacao = Convert.ToInt32(reader["Qualificacao"]); 
+                        result.Qualificacao = Convert.ToInt32(reader["Qualificacao"]);
                         result.Ativo = Convert.ToBoolean(reader["Ativo"]);
-                        result.Observacoes = reader["Observacoes"].ToString(); 
+                        result.Observacoes = reader["Observacoes"].ToString();
                     }
                 }
                 catch (Exception e)
@@ -106,12 +106,12 @@ namespace RemessaFaccao.DAL.Repositories.Interfaces
                 try
                 {
                     connection.Open();
-                    SqlCommand command = new SqlCommand("SELECT COUNT(FaccaoId) result FROM Faccao; "); 
+                    SqlCommand command = new SqlCommand("SELECT COUNT(FaccaoId) result FROM Faccao; ");
                     command.Connection = connection;
                     command.CommandType = CommandType.Text;
                     SqlDataReader reader = command.ExecuteReader();
-                    result = Convert.ToInt32(reader["result"]); 
-                    
+                    result = Convert.ToInt32(reader["result"]);
+
                 }
                 catch (Exception e)
                 {
@@ -119,16 +119,49 @@ namespace RemessaFaccao.DAL.Repositories.Interfaces
                 }
                 finally
                 {
-                    if(connection.State == ConnectionState.Open)
+                    if (connection.State == ConnectionState.Open)
                         connection.Close();
                 }
-                return result; 
+                return result;
             }
         }
 
         public bool Insert(Faccao faccao)
         {
-            throw new NotImplementedException();
+            bool result = false;
+            SqlConnection connection = new SqlConnection(_connection.SQLString);
+            using (connection)
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("dbo.FaccaoInsert");
+                    command.Connection = connection;
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add("@Nome", SqlDbType.VarChar).Value = faccao.Nome;
+                    command.Parameters.Add("@Endereco", SqlDbType.VarChar).Value = faccao.Endereco;
+                    command.Parameters.Add("@Email", SqlDbType.VarChar).Value = faccao.Email;
+                    command.Parameters.Add("@Telefone1", SqlDbType.VarChar).Value = faccao.Telefone1;
+                    command.Parameters.Add("@Telefone2", SqlDbType.VarChar).Value = faccao.Telefone2;
+                    command.Parameters.Add("@FormaDePagamento", SqlDbType.VarChar).Value = faccao.FormaDePagamento;
+                    command.Parameters.Add("@Qualificacao", SqlDbType.Int).Value = faccao.Qualificacao;
+                    command.Parameters.Add("@Ativo", SqlDbType.Bit).Value = faccao.Ativo;
+                    command.Parameters.Add("@Observacoes", SqlDbType.NVarChar).Value = faccao.Observacoes;
+
+                    if (Convert.ToInt32(command.ExecuteNonQuery()) != 0)
+                        result = true;
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Erro ao acessar informações do banco de dados. " + e.Message);
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                        connection.Close();
+                }
+                return result;
+            }
         }
 
         public bool Update(Faccao faccao)
