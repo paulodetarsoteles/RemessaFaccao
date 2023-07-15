aa6acafdec48dc7a6e484dcc89bd1f57a23ed8ed
Paulo de Tarso <potassio@hotmail.com>
2023-07-15 11:52:57 -0300

    Implementado o upload de imagens na controller Imagens

--- RemessaFaccao/RemessaFaccao.Web/Controllers/ImagensController.cs
+++ RemessaFaccao/RemessaFaccao.Web/Controllers/ImagensController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using RemessaFaccao.DAL.Setting;
+using System.Reflection;
 
 namespace RemessaFaccao.Web.Controllers
 {
@@ -14,14 +15,65 @@ namespace RemessaFaccao.Web.Controllers
         public ImagensController(IWebHostEnvironment webHostEnvironment, IOptions<PathFiles> pathFiles)
         {
             _webHostEnvironment = webHostEnvironment;
-            _pathFiles = pathFiles.Value; 
+            _pathFiles = pathFiles.Value;
         }
 
         //GET: ImagensController
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Index() => View();
+
+        //POST: ImagensController/UploadImagens
+        [HttpPost]
+        public async Task<IActionResult> UploadImagens(List<IFormFile> files)
         {
-            return View();
+            try
+            {
+                if (files is null || files.Count == 0)
+                {
+                    ViewData["Erro"] = "Nenhuma imagem não selecionada.";
+                    return View(ViewData);
+                }
+
+                if (files.Count > 5)
+                {
+                    ViewData["Erro"] = "Quantidade de imagens não pode ser maior que 5.";
+                    return View(ViewData);
+                }
+
+                if (files.Sum(f => f.Length) > 10000000)
+                {
+                    ViewData["Erro"] = "Tamanho máximo das imagens ultrapassou 10Mb.";
+                    return View(ViewData);
+                }
+
+                List<string> filePathsName = new();
+
+                foreach (IFormFile file in files)
+                {
+                    if (file.FileName.Contains(".jpg") || file.FileName.Contains(".jpeg") || file.FileName.Contains(".gif") || file.FileName.Contains(".png") || file.FileName.Contains(".bmp"))
+                    {
+                        string fileNameWithPath = $"{_pathFiles}\\{file.FileName}";
+
+                        filePathsName.Add(fileNameWithPath);
+
+                        using FileStream stream = new(fileNameWithPath, FileMode.Create);
+
+                        await file.CopyToAsync(stream);
+                    }
+                }
+
+                ViewData["Resultado"] = $"{files.Count} imagens enviadas.";
+
+                ViewBag.Arquivos = filePathsName;
+
+                return View(ViewData);
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", "Erro inesperado ao enviar imagens!");
+                ConfigHelper.WriteLog(ConfigHelper.PathOutLog("ImagemController"), $"Erro ao acessar {MethodBase.GetCurrentMethod()}. {e.StackTrace} - {DateTime.Now}");
+                return View();
+            }
         }
     }
 }
